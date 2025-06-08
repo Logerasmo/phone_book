6e96abae32b9908bd5ae135578ec4bcc8b83555b
logerasmo <malishev30k@gmail.com>
2025-06-09 01:34:15 +0300

    findByNumber implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PhoneBook {
     private final Map<String, String> numbers = new HashMap<>();
@@ -10,6 +11,16 @@ public class PhoneBook {
         return numbers.size();
     }
     public String findByNumber(String number){
+        if (!numbers.containsValue(number)){
+            return null;
+        }
+
+        for (Map.Entry s : numbers.entrySet()
+                .stream()
+                .filter(x -> x.getValue().equals(number))
+                .collect(Collectors.toSet())){
+            return (String) s.getKey();
+        }
         return null;
     }
 }
