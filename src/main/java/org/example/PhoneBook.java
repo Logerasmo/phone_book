1924e49814f2203aedf7003d98be14430a411fd4
logerasmo <malishev30k@gmail.com>
2025-06-09 01:11:36 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> numbers = new HashMap<>();
     public int add(String name, String number){
-        return 0;
+        numbers.put(name, number);
+        return numbers.size();
     }
 }
