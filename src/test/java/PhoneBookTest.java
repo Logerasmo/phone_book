c7da07290592cb3c1176aec904441c88439079a1
logerasmo <malishev30k@gmail.com>
2025-06-09 01:09:32 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,14 @@
+import org.example.PhoneBook;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class PhoneBookTest {
+    @Test
+    public void addTest(){
+        PhoneBook phoneBook = new PhoneBook();
+        Assert.assertEquals(1, phoneBook.add("Joe", "89541233456"));
+        Assert.assertEquals(2, phoneBook.add("Rob", "89541233457"));
+        Assert.assertEquals(2, phoneBook.add("Joe", "89541233456"));
+        Assert.assertEquals(3, phoneBook.add("Remo", "89541233450"));
+    }
 }
